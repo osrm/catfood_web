97b0ef35bb59f9fa2eb1688841c8cd8778047784
osrm <90407222+osrm@users.noreply.github.com>
2026-09-01 21:05:55 +0900

    style: load home theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './refinements.css'
 import './warm-editorial.css'
+import './home.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

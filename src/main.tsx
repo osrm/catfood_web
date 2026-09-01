59c7407f202dd57531ee329e83bdd3fd7d5b497e
osrm <90407222+osrm@users.noreply.github.com>
2026-09-01 20:44:13 +0900

    style: apply warm editorial theme to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './refinements.css'
+import './warm-editorial.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

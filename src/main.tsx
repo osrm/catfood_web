edc2bd10f7a5f6431d5fe091685ca32c1e012f90
osrm <90407222+osrm@users.noreply.github.com>
2026-09-07 12:20:40 +0900

    style: load explore package polish

--- src/main.tsx
+++ src/main.tsx
@@ -27,6 +27,7 @@ import './detail-consumer-refresh.css'
 import './document-scroll-fix.css'
 import './stitch-final-polish.css'
 import './stitch-final-polish-fixes.css'
+import './explore-package-polish.css'
 
 installDemoPreviewFetch()
 installRealVisualPreviewFetch()
